89b7647694f91790c48b7dfd83922d5cf29cb783
modraws <114813543+modraws@users.noreply.github.com>
2024-12-23 14:15:14 +0100

    sorry tyler lol

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 from colorama import Fore, Style, init
 init()
 import importlib.util
-import os
 
 # user guide for making your own
 # addon/addon2 will set replacee/replacement to a string. addon can be a list (optional), addon2 cant
@@ -20,9 +19,9 @@ def get_valid_input(prompt, valid_values=None):
             if valid_values is None or int(user_input) in valid_values:
                 return int(user_input)
             else:
-                print(f"{Fore.RED}Invalid option. Please choose from {valid_values}.{Style.RESET_ALL}")
+                print(f"{Fore.MAGENTA}\nInvalid option. Please choose from {valid_values}.\n{Style.RESET_ALL}")
         except ValueError:
-            print(f"{Fore.RED}Invalid input. Please enter a valid number.{Style.RESET_ALL}")
+            print(f"{Fore.MAGENTA}\nInvalid input. Please enter a valid number.\n{Style.RESET_ALL}")
 
 
 def push(json_data, start_key, start_key2, addon, addon2, skip):
@@ -36,31 +35,11 @@ def push(json_data, start_key, start_key2, addon, addon2, skip):
         print(f"Error: The file {backbone_module} does not contain a `backbone` function.")
 
 
-def bloxstrap():
-    base_path = os.path.join(os.getenv('LOCALAPPDATA'), 'Bloxstrap', 'Modifications')
-    nested_folders = ["PlatformContent", "pc", "textures", "sky"]
-
-    if not os.path.exists(base_path):
-        print(f"{Fore.RED}bloxstrap not found{Style.RESET_ALL}")
-    else:
-        path = base_path
-        for folder in nested_folders:
-            path = os.path.join(path, folder)
-            if not os.path.exists(path):
-                os.makedirs(path)
-                print(f"Created folder: {path}")
-            else:
-                print(f"Folder already exists: {path}")
-
-        print("All folders created successfully! Import your skyboxes into the opened folder.")
-        os.startfile(path)
-
-
 def run(json_data, start_key, start_key2, addon, addon2, skip):
-    history = []
 
     while True:
-        options = input(f"""asset replacements:
+        options = get_valid_input(f"""Asset replacements:
+0:  {Fore.LIGHTMAGENTA_EX}Custom{Style.RESET_ALL}
 1:  {Fore.LIGHTMAGENTA_EX}Sights{Style.RESET_ALL}
 2:  {Fore.LIGHTMAGENTA_EX}Sleeves{Style.RESET_ALL}
 3:  {Fore.LIGHTMAGENTA_EX}Custom skyboxes (not added yet,,, maybe in future){Style.RESET_ALL}
@@ -68,31 +47,32 @@ def run(json_data, start_key, start_key2, addon, addon2, skip):
 5:  {Fore.LIGHTMAGENTA_EX}Hit tweaks{Style.RESET_ALL}
 6:  {Fore.LIGHTMAGENTA_EX}Grenade tweaks{Style.RESET_ALL}
 7:  {Fore.LIGHTMAGENTA_EX}Start-up swoosh{Style.RESET_ALL}
-Type 'back' to return to the previous menu
-: """).strip().lower()
+Type 'back' to return to the previous menu.
+: """,
+                        valid_values=[0, 1, 2, 3, 4, 5, 6, 7]
+        )
 
         if options == 'back':
-            if history:
-                json_data, start_key, start_key2, addon, addon2, skip = history.pop()
-                print(f"{Fore.CYAN}Restored previous selection.{Style.RESET_ALL}")
-            else:
-                print(f"{Fore.RED}No previous selection to return to.{Style.RESET_ALL}")
-            continue
-
+            print(f"{Fore.CYAN}\nReturning to main menu.{Style.RESET_ALL}")
+            skip = True
+            return json_data, start_key, start_key2, addon, addon2, skip
+        
         try:
-            match int(options):
+            match options:
+                case 0:
+                    return json_data, start_key, start_key2, addon, addon2, skip
                 case 1:
-                    history.append((json_data, start_key, start_key2, addon, addon2, skip))
 
                     while True:
                         sight_option = get_valid_input(
                             f"\nEnter sight option:\n"
                             f"1: {Fore.LIGHTMAGENTA_EX}Reticle tweaks{Style.RESET_ALL}\n"
-                            f"2: {Fore.LIGHTMAGENTA_EX}Sight model tweaks{Style.RESET_ALL}\n",
+                            f"2: {Fore.LIGHTMAGENTA_EX}Sight model tweaks{Style.RESET_ALL}\n"
+                            f"Type 'back' to return to the previous menu.\n: ",
                             valid_values=[1, 2]
                         )
                         if sight_option == 'back':
-                            print(f"{Fore.CYAN}Returning to the main menu.{Style.RESET_ALL}")
+                            print(f"{Fore.CYAN}Returning to Asset replacements.\n{Style.RESET_ALL}")
                             break
 
                         match sight_option:
@@ -104,7 +84,8 @@ Type 'back' to return to the previous menu
                                 while True:
                                     sightedit = get_valid_input(
                                         f"\nChoose a sight you'd like to edit:\n"
-                                        f"1: {Fore.LIGHTMAGENTA_EX}Izhmash{Style.RESET_ALL}\n",
+                                        f"1: {Fore.LIGHTMAGENTA_EX}Izhmash{Style.RESET_ALL}\n"
+                                        f"Type 'back' to return to the previous menu.\n: ",
                                         valid_values=[1]
                                     )
                                     if sightedit == 'back':
@@ -118,11 +99,12 @@ Type 'back' to return to the previous menu
                                                     f"1: {Fore.LIGHTMAGENTA_EX}Rear{Style.RESET_ALL}\n"
                                                     f"2: {Fore.LIGHTMAGENTA_EX}Rear (detail){Style.RESET_ALL}\n"
                                                     f"3: {Fore.LIGHTMAGENTA_EX}Front{Style.RESET_ALL}\n"
-                                                    f"4: {Fore.LIGHTMAGENTA_EX}Front (detail){Style.RESET_ALL}\n",
+                                                    f"4: {Fore.LIGHTMAGENTA_EX}Front (detail){Style.RESET_ALL}\n"
+                                                    f"Type 'back' to return to the previous menu.\n: ",
                                                     valid_values=[1, 2, 3, 4]
                                                 )
                                                 if izhmashedit == 'back':
-                                                    print(f"{Fore.CYAN}Returning to sight options.{Style.RESET_ALL}")
+                                                    print(f"{Fore.CYAN}Returning to sight model tweaks.{Style.RESET_ALL}")
                                                     break
                                                 match izhmashedit:
                                                     case 1:
@@ -146,67 +128,58 @@ Type 'back' to return to the previous menu
                     start_key2 = "dumbstupidness skins"
                     return json_data, start_key, start_key2, addon, addon2, skip
                 case 3:
-                    history.append((json_data, start_key, start_key2, addon, addon2, skip))
                     print(f"{Fore.RED}I said it wasn't added yet, goofball.{Style.RESET_ALL}")            
                 case 4:
                     start_key = "gun sounds"
                     start_key2 = "dumbstupidness sounds"
                     return json_data, start_key, start_key2, addon, addon2, skip
                 case 5:
-                    history.append((json_data, start_key, start_key2, addon, addon2, skip))
-
                     while True:
                         hit_option = get_valid_input(
                             f"\nEnter hit option:\n"
                             f"1: {Fore.LIGHTMAGENTA_EX}Hit sounds{Style.RESET_ALL}\n"
-                            f"2: {Fore.LIGHTMAGENTA_EX}Kill sounds{Style.RESET_ALL}\n: ",
+                            f"2: {Fore.LIGHTMAGENTA_EX}Kill sounds{Style.RESET_ALL}\n: "
+                            f"Type 'back' to return to the previous menu.\n: ",
                             valid_values=[1, 2]
                         )
                         if hit_option == 'back':
-                            print(f"{Fore.CYAN}Returning to the main menu.{Style.RESET_ALL}")
+                            print(f"{Fore.CYAN}Returning to Asset replacements.\n{Style.RESET_ALL}")
                             break
 
                         match hit_option:
                             case 1:
                                 addon = "a177d2c00abd3e550b873d76c97ad960"
                                 start_key2 = "dumbstupidness sounds"
+                                return json_data, start_key, start_key2, addon, addon2, skip
                             case 2:
                                 start_key = "kill default"
                                 addon2 = "dumbstupidness sounds"
-                            case _:
-                                print(f"{Fore.RED}Invalid option.{Style.RESET_ALL}")
-                        return json_data, start_key, start_key2, addon, addon2, skip
+                                return json_data, start_key, start_key2, addon, addon2, skip
                 case 6:
-                    history.append((json_data, start_key, start_key2, addon, addon2, skip))
-
                     while True:
                         grenade_option = get_valid_input(
                             f"\nEnter grenade option:\n"
                             f"1: {Fore.LIGHTMAGENTA_EX}Explosion sound{Style.RESET_ALL}\n"
-                            f"2: {Fore.LIGHTMAGENTA_EX}Grenade sound{Style.RESET_ALL}\n: ",
+                            f"2: {Fore.LIGHTMAGENTA_EX}Grenade sound{Style.RESET_ALL}\n: "
+                            f"Type 'back' to return to the previous menu.\n: ",
                             valid_values=[1, 2]
                         )
                         if grenade_option == 'back':
-                            print(f"{Fore.CYAN}Returning to the main menu.{Style.RESET_ALL}")
+                            print(f"{Fore.CYAN}Returning to Asset replacements.\n{Style.RESET_ALL}")
                             break
 
                         match grenade_option:
                             case 1:
                                 start_key = "explosions default"
                                 start_key2 = "dumbstupidness sounds"
+                                return json_data, start_key, start_key2, addon, addon2, skip
                             case 2:
                                 start_key = "grenade sound"
                                 start_key2 = "dumbstupidness sounds"
-                            case _:
-                                print(f"{Fore.RED}Invalid option.{Style.RESET_ALL}")
-                        return json_data, start_key, start_key2, addon, addon2, skip
+                                return json_data, start_key, start_key2, addon, addon2, skip
                 case 7:
                     addon = "fdbab7608661dafc624b781910efd953"
                     start_key2 = "replacement sounds"
                     return json_data, start_key, start_key2, addon, addon2, skip
-                case _:
-                    print(f"{Fore.RED}Invalid number.{Style.RESET_ALL}")
-        except ValueError:
-            print(f"{Fore.RED}Invalid input. Please enter a valid number.{Style.RESET_ALL}")
         except Exception as e:
-            print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+            print(f"{Fore.MAGENTA}An error occurred: {e}{Style.RESET_ALL}")
