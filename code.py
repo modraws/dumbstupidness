ec8180a10c03f8873928aff0eed4f56f9b57b5a8
modraws <114813543+modraws@users.noreply.github.com>
2024-12-23 15:50:18 +0100

    updootle

--- code.py
+++ code.py
@@ -5,8 +5,8 @@ import importlib.util
 # user guide for making your own
 # addon/addon2 will set replacee/replacement to a string. addon can be a list (optional), addon2 cant
 # start_key/start_key2 will start the user in a json from a set position (make sure the set position doesnt share a name or ill go to the first one)
-# return json_data, start_key, start_key2, addon, addon2, skip finishes an area and returns all the data back and finishes this codes use
-# add a line push(json_data, start_key, start_key2, addon, addon2, skip) for doing multiple changes of seperate things in the same case
+# return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names finishes an area and returns all the data back and finishes this codes use
+# add a line push(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names) for doing multiple changes of seperate things in the same case
 # if you want to skip then set skip to True
 # leaving empty and only returning will make the user enter 2 from the json starting from the top
 
@@ -24,18 +24,18 @@ def get_valid_input(prompt, valid_values=None):
             print(f"{Fore.MAGENTA}\nInvalid input. Please enter a valid number.\n{Style.RESET_ALL}")
 
 
-def push(json_data, start_key, start_key2, addon, addon2, skip):
+def push(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names):
     spec = importlib.util.spec_from_file_location("backbone_module", "main.py")
     backbone_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(backbone_module)
 
     if hasattr(backbone_module, "backbone"):
-        json_data, start_key, start_key2, addon, addon2, skip = backbone_module.backbone(json_data, start_key, start_key2, addon, addon2, skip)
+        json_data, start_key, start_key2, addon, addon2, skip = backbone_module.backbone(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names)
     else:
         print(f"Error: The file {backbone_module} does not contain a `backbone` function.")
 
 
-def run(json_data, start_key, start_key2, addon, addon2, skip):
+def run(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names):
 
     while True:
         options = get_valid_input(f"""Asset replacements:
@@ -55,12 +55,12 @@ Type 'back' to return to the previous menu.
         if options == 'back':
             print(f"{Fore.CYAN}\nReturning to main menu.{Style.RESET_ALL}")
             skip = True
-            return json_data, start_key, start_key2, addon, addon2, skip
+            return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
         
         try:
             match options:
                 case 0:
-                    return json_data, start_key, start_key2, addon, addon2, skip
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                 case 1:
 
                     while True:
@@ -79,7 +79,7 @@ Type 'back' to return to the previous menu.
                             case 1:
                                 start_key = "reticles"
                                 start_key2 = "dumbstupidness reticles"
-                                return json_data, start_key, start_key2, addon, addon2, skip
+                                return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                             case 2:
                                 while True:
                                     sightedit = get_valid_input(
@@ -110,29 +110,29 @@ Type 'back' to return to the previous menu.
                                                     case 1:
                                                         addon = "0d910831a0fe0b50fe2c50125e7b8f52"
                                                         addon2 = "058e54ef5ad3fb914c34a6f446a36702"
-                                                        return json_data, start_key, start_key2, addon, addon2, skip
+                                                        return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                                                     case 2:
                                                         addon = "edc7f492f5384f4db6f306bf8be26341"
                                                         addon2 = "058e54ef5ad3fb914c34a6f446a36702"
-                                                        return json_data, start_key, start_key2, addon, addon2, skip
+                                                        return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                                                     case 3:
                                                         addon = "80e288c6f1ee8597b93ca2e748d1ab68"
                                                         addon2 = "058e54ef5ad3fb914c34a6f446a36702"
-                                                        return json_data, start_key, start_key2, addon, addon2, skip
+                                                        return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                                                     case 4:
                                                         addon = "ca6de453e3071d01016a0fb31d68648d"
                                                         addon2 = "058e54ef5ad3fb914c34a6f446a36702"
-                                                        return json_data, start_key, start_key2, addon, addon2, skip
+                                                        return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                 case 2:
                     addon = "8813bbc8c0f7c0901fc38c1c85935fec"
                     start_key2 = "dumbstupidness skins"
-                    return json_data, start_key, start_key2, addon, addon2, skip
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                 case 3:
                     print(f"{Fore.RED}I said it wasn't added yet, goofball.{Style.RESET_ALL}")            
                 case 4:
                     start_key = "gun sounds"
                     start_key2 = "dumbstupidness sounds"
-                    return json_data, start_key, start_key2, addon, addon2, skip
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                 case 5:
                     while True:
                         hit_option = get_valid_input(
@@ -150,11 +150,11 @@ Type 'back' to return to the previous menu.
                             case 1:
                                 addon = "a177d2c00abd3e550b873d76c97ad960"
                                 start_key2 = "dumbstupidness sounds"
-                                return json_data, start_key, start_key2, addon, addon2, skip
+                                return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                             case 2:
                                 start_key = "kill default"
                                 addon2 = "dumbstupidness sounds"
-                                return json_data, start_key, start_key2, addon, addon2, skip
+                                return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                 case 6:
                     while True:
                         grenade_option = get_valid_input(
@@ -172,14 +172,14 @@ Type 'back' to return to the previous menu.
                             case 1:
                                 start_key = "explosions default"
                                 start_key2 = "dumbstupidness sounds"
-                                return json_data, start_key, start_key2, addon, addon2, skip
+                                return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                             case 2:
                                 start_key = "grenade sound"
                                 start_key2 = "dumbstupidness sounds"
-                                return json_data, start_key, start_key2, addon, addon2, skip
+                                return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                 case 7:
                     addon = "fdbab7608661dafc624b781910efd953"
                     start_key2 = "replacement sounds"
-                    return json_data, start_key, start_key2, addon, addon2, skip
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
         except Exception as e:
             print(f"{Fore.MAGENTA}An error occurred: {e}{Style.RESET_ALL}")
